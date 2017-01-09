91df8cdf75fba11fbb9eaf05f9a7ca89b6bec810
Sam Burrell <sburrell@Sams-MacBook-Air-4.local>
2017-01-09 11:23:46 -0600

    Final

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var total = 0.0
+    var people = 1
+    
+    @IBOutlet var UITapGestureRecognizer: UITapGestureRecognizer!
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var tipTotal: UILabel!
+    @IBOutlet weak var totalCost: UILabel!
+    @IBOutlet weak var peopleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "greenImage.png")
+        self.view.insertSubview(backgroundImage, at: 0)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func tipTen(_ sender: AnyObject) {
+        calc(x: 0.10)
     }
-
-
+    
+    @IBAction func fifteenTip(_ sender: AnyObject) {
+        calc(x: 0.15)
+    }
+    
+    @IBAction func tipTwenty(_ sender: AnyObject) {
+        calc(x: 0.20)
+    }
+    
+    @IBAction func tipTwentyFive(_ sender: AnyObject) {
+        calc(x: 0.25)
+    }
+    
+    @IBAction func buttonDown(_ sender: AnyObject) {
+        if people == 1 {
+            return
+        }
+        people -= 1
+        peopleLabel.text = String(people)
+    }
+    
+    @IBAction func buttonUp(_ sender: AnyObject) {
+        people += 1
+        peopleLabel.text = String(people)
+    }
+    
+    
+    
+    
+    
+    
+    func calc(x : Double)
+    {
+        var total = Double (billAmount.text!)
+        var tip = total! * x
+        var cost = total! + tip
+        tip = tip / Double (people)
+        cost = cost / Double (people)
+        tipTotal.text = "$" + String(format :"%.2f",tip)
+        totalCost.text = "$" + String(format : "%.2f" ,cost)
+        
+    }
 }
-
